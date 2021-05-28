ad0ae4332923a385d9d613a7d75c3ee8c55f987f
Dyneteve <dyneteve@hentaios.com>
2021-05-28 21:44:05 +0200

    techpack: drm/msm/sde: Don't early wakeup on lcd devices
    - Idle timer might not be set in some cases, we want fast wakeup on amoled
    
    Signed-off-by: Dyneteve <dyneteve@hentaios.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>
    Co-authored-by: Flicker372 <flicker372@outlook.com>

--- techpack/display/msm/sde/sde_crtc.c
+++ techpack/display/msm/sde/sde_crtc.c
@@ -6681,6 +6681,11 @@ static void __sde_crtc_idle_notify_work(struct kthread_work *work)
 /*
  * __sde_crtc_early_wakeup_work - trigger early wakeup from user space
  */
+#if !defined(CONFIG_MACH_XIAOMI_APOLLO) && \
+    !defined(CONFIG_MACH_XIAOMI_DAGU) && \
+    !defined(CONFIG_MACH_XIAOMI_ELISH) && \
+    !defined(CONFIG_MACH_XIAOMI_ENUMA) && \
+    !defined(CONFIG_MACH_XIAOMI_PIPA)
 static void __sde_crtc_early_wakeup_work(struct kthread_work *work)
 {
 	struct sde_crtc *sde_crtc = container_of(work, struct sde_crtc,
@@ -6711,6 +6716,7 @@ static void __sde_crtc_early_wakeup_work(struct kthread_work *work)
 	sde_kms = to_sde_kms(priv->kms);
 	sde_kms_trigger_early_wakeup(sde_kms, crtc);
 }
+#endif
 
 /* initialize crtc */
 struct drm_crtc *sde_crtc_init(struct drm_device *dev, struct drm_plane *plane)
@@ -6804,8 +6810,14 @@ struct drm_crtc *sde_crtc_init(struct drm_device *dev, struct drm_plane *plane)
 
 	kthread_init_delayed_work(&sde_crtc->idle_notify_work,
 					__sde_crtc_idle_notify_work);
+#if !defined(CONFIG_MACH_XIAOMI_APOLLO) && \
+    !defined(CONFIG_MACH_XIAOMI_DAGU) && \
+    !defined(CONFIG_MACH_XIAOMI_ELISH) && \
+    !defined(CONFIG_MACH_XIAOMI_ENUMA) && \
+    !defined(CONFIG_MACH_XIAOMI_PIPA)
 	kthread_init_work(&sde_crtc->early_wakeup_work,
 					__sde_crtc_early_wakeup_work);
+#endif
 
 	SDE_DEBUG("crtc=%d new_llcc=%d, old_llcc=%d\n",
 		crtc->base.id,
